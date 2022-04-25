1afcc090e5b84ee17b485916fe37651369bee21e
Russell <russellmorbey@hotmail.co.uk>
2022-04-25 15:32:19 +0100

    Challenge 4. Added balance test.

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,5 +1,16 @@
 describe Oystercard do
-  
+  it 'can show a balance' do
+
+    expect(subject.balance).to eq 0
+
+    
+    # User story 1
+    # In order to use public transport
+    # As a customer
+    # I want money on my card
+
+  end
+
 
 
 end
